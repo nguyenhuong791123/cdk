159dbb3475b073e7c80335f7c51b5e4fa2231485
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2020-02-05 23:14:16 +0900

    Infra AutoScale Loadbalancer

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,13 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
-
 import o from '../utils/setting.json';
 
 const app = new cdk.App();
-// new CdkStack(app, "InsCdkStack", {
-//     env: { account: "710626597572", region: "ap-northeast-1" }
-// });
-new CdkStack(app, o.stack_name, {
-    env: { account: o.account_id, region: o.region }
+new CdkStack(app, o.STACK_NAME, {
+    env: { account: o.ACCOUNT_ID, region: o.REGION }
 });
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -3,15 +3,8 @@ import ec2 = require('@aws-cdk/aws-ec2');
 import elbv2 = require("@aws-cdk/aws-elasticloadbalancingv2");
 import { AutoScalingGroup } from "@aws-cdk/aws-autoscaling";
 import { ManagedPolicy, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
-import {
-  AmazonLinuxImage,
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  ISecurityGroup,
-  IVpc,
-  SubnetType
-} from "@aws-cdk/aws-ec2";
+import route53 = require('@aws-cdk/aws-route53');
+import * as balancer from './balancer';
 
 import o from '../utils/setting.json';
 
@@ -19,68 +12,64 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "CdkVPC", {
-      cidr: o.cidr,
+    const vpc = new ec2.Vpc(this, o.VPC_NAME, {
+      cidr: o.CIDR,
       subnetConfiguration: [
-          { name: "Cdk-Public", cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC }
-          ,{ name: "Cdk-Private", cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE }
+          { name: o.VPC_NAME + "-Public", cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC }
+          ,{ name: o.VPC_NAME + "-Private", cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE }
       ]
     });
 
-    vpc.node.applyAspect(new cdk.Tag("Name", "Cdk-Vpc"));
+    vpc.node.applyAspect(new cdk.Tag("Name", "CdkApplyAspectVpc"));
     for (let subnet of vpc.publicSubnets) {
-      subnet.node.applyAspect(new cdk.Tag("Name", `${subnet.node.id.replace(/Subnet[0-9]$/, "")}-${subnet.availabilityZone}`));
+      subnet.node.applyAspect(new cdk.Tag("Name", o.SUBNET_NAME + `${subnet.node.id.replace(/Subnet[0-9]$/, "")}-${subnet.availabilityZone}`));
     }
     for (let subnet of vpc.privateSubnets) {
-      subnet.node.applyAspect(new cdk.Tag("Name", `${subnet.node.id.replace(/Subnet[0-9]$/, "")}-${subnet.availabilityZone}`));
+      subnet.node.applyAspect(new cdk.Tag("Name", o.SUBNET_NAME + `${subnet.node.id.replace(/Subnet[0-9]$/, "")}-${subnet.availabilityZone}`));
     }
 
-    const cdkSG = new ec2.SecurityGroup(this, o.sg_name, {
+    const cdkSG = new ec2.SecurityGroup(this, o.SECURITY_GROUP_NAME, {
       allowAllOutbound: true,
-      securityGroupName: o.sg_name,
+      securityGroupName: o.SECURITY_GROUP_NAME,
       vpc: vpc
     });
-    cdkSG.node.applyAspect(new cdk.Tag("Name", o.ec2_name));
+    cdkSG.addIngressRule(cdkSG, ec2.Port.tcp(80));
+    cdkSG.node.applyAspect(new cdk.Tag("Name", o.EC2_NAME));
 
     const cmd = ec2.UserData.forLinux();
     const SSM_AGENT_RPM='https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm';
-    cmd.addCommands('echo', 'Cdk Run !!!');
     cmd.addCommands(`yum install -y ${SSM_AGENT_RPM}`, 'restart amazon-ssm-agent');
     cmd.addCommands("amazon-linux-extras install -y nginx1.12", "systemctl enable nginx", "systemctl start nginx");
 
-    const instanceRole = new Role(this, "IamRole", {
-      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
-      roleName: `Cdk-iam-role`,
-      managedPolicies: [
-        ManagedPolicy.fromAwsManagedPolicyName(
-          "service-role/AmazonEC2RoleforSSM"
-        )
-      ]
+    const insRole = new Role(this, o.ROLE_NAME, {
+      assumedBy: new ServicePrincipal(o.EC2_DOMAIN)
+      ,roleName: o.ROLE_NAME
+      ,managedPolicies: [ ManagedPolicy.fromAwsManagedPolicyName(o.AWS_EC2_ROLE) ]
     });
-    const targets: elbv2.IApplicationLoadBalancerTarget[] = [];
-    const asg = new AutoScalingGroup(this, "ASG", {
-      vpc: vpc
-      ,instanceType: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)
-      ,machineImage: new AmazonLinuxImage()
-      ,allowAllOutbound: true
-      ,role: instanceRole
-    });
-    asg.addSecurityGroup(cdkSG);
-    asg.addUserData(
-      // "yum -y update",
-      "yum -y install nginx",
-      "systemctl enable nginx",
-      "systemctl start nginx"
+    const asg = new AutoScalingGroup(
+      this
+      ,o.AUTO_SCALE_GROUP_NAME
+      ,{
+        vpc: vpc
+        ,instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO)
+        ,machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 })
+        ,minCapacity: 2
+        ,allowAllOutbound: true
+        ,role: insRole
+      }
     );
-    targets.push(asg);
+    asg.addSecurityGroup(cdkSG);
+    asg.addUserData(cmd.render());
+    const albt: elbv2.IApplicationLoadBalancerTarget[] = [];
+    albt.push(asg);
 
     // for (let privateSubnet of vpc.privateSubnets) {
         // const instance = new ec2.CfnInstance(
         //   this
-        //   ,`WebInstance-${privateSubnet.node.id}`
+        //   ,`ec2-${privateSubnet.node.id}`
         //   ,{
         //     imageId: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }).getImage(this).imageId
-        //     ,instanceType: o.ec2_type
+        //     ,instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO)
         //     // ,keyName: node.tryGetContext("key")
         //     ,subnetId: privateSubnet.subnetId
         //     ,securityGroupIds: [ cdkSG.securityGroupId ]
@@ -91,7 +80,10 @@ export class CdkStack extends cdk.Stack {
         // targets.push(new elbv2.InstanceTarget(instance.ref.toString()));
     // }
 
-    const alb = new elbv2.ApplicationLoadBalancer(this, "Alb", { vpc: vpc, internetFacing: true, loadBalancerName: "Cdk-Alb" });
+    const alb = new elbv2.ApplicationLoadBalancer(
+      this
+      ,o.LOADBALANE_NAME
+      ,{ vpc: vpc, internetFacing: true, loadBalancerName: o.LOADBALANE_NAME });
     alb.addListener(
       "Listener"
       ,{
@@ -107,15 +99,26 @@ export class CdkStack extends cdk.Stack {
               ,port: 80
               ,protocol: elbv2.ApplicationProtocol.HTTP
               ,healthCheck: { path: "/index.html", port: "80", protocol: elbv2.Protocol.HTTP }
-              ,targetGroupName: "Cdk-Web-TargetGroup"
-              ,targets: targets
+              ,targetGroupName: o.TARGET_GROUP_NAME
+              ,targets: albt
             }
           )
       ]
     });
 
-    const albSG = ec2.SecurityGroup.fromSecurityGroupId(this, "AlbSG", cdk.Fn.select(0, alb.loadBalancerSecurityGroups));
+    const albSG = ec2.SecurityGroup.fromSecurityGroupId(this, o.LOADBALANE_SECURITY_GROUP_NAME, cdk.Fn.select(0, alb.loadBalancerSecurityGroups));
     albSG.addEgressRule(cdkSG, ec2.Port.tcp(80));
     cdkSG.addIngressRule(albSG, ec2.Port.tcp(80));
+
+    const zone = route53.HostedZone.fromLookup(this, o.HOST_ZONE_NAME, { "domainName": o.DOMAIN });
+    new route53.ARecord(
+      this
+      ,o.ROUTE53_RECORD_NAME
+      ,{
+        zone
+        ,recordName: o.DOMAIN
+        ,target: route53.RecordTarget.fromAlias(new balancer.LoadBalancerTarget(alb))
+      }
+    );
   }
 }
